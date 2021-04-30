6c59e1fabcec49088ce689b0e42c700ec01e4b59
Varphone Wong <varphone@qq.com>
2021-04-30 11:25:08 +0800

    Change created_at display with local timezone
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,7 +1,14 @@
 use crate::db;
+use chrono::{Local, TimeZone};
 use rocket::Route;
 use rocket_contrib::templates::Template;
 use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+struct FileExt {
+    file: db::models::File,
+    created_at_str: String,
+}
 
 #[derive(Debug, Serialize)]
 struct HeaderCtx {
@@ -11,7 +18,7 @@ struct HeaderCtx {
 #[derive(Debug, Serialize)]
 struct IndexCtx {
     header: HeaderCtx,
-    list: Vec<db::models::File>,
+    list: Vec<FileExt>,
     size: usize,
 }
 
@@ -19,11 +26,23 @@ struct IndexCtx {
 pub(crate) fn index() -> Template {
     match db::files::all_files(&db::establish_connection()) {
         Ok(v) => {
-            let size = v.len();
+            let new_list: Vec<FileExt> = v
+                .into_iter()
+                .map(|file| {
+                    let created_at_local = Local.from_utc_datetime(&file.created_at);
+                    let created_at_str = format!("{}", created_at_local.format("%F %T"));
+                    FileExt {
+                        file,
+                        created_at_str,
+                    }
+                })
+                .collect();
+
+            let size = new_list.len();
             let header_ctx = HeaderCtx { show_upload: true };
             let index_ctx = IndexCtx {
                 header: header_ctx,
-                list: v,
+                list: new_list,
                 size,
             };
             Template::render("index", index_ctx)
